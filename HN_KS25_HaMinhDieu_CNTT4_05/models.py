15b858b97ebf346c2f6848642c65d0cd4b76b550
DieuuHaa <haminhdieu336@gmail.com>
2026-07-13 14:27:14 +0700

    Update models.py

--- HN_KS25_HaMinhDieu_CNTT4_05/models.py
+++ HN_KS25_HaMinhDieu_CNTT4_05/models.py
@@ -4,10 +4,10 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class LibraryModel(Base):
-    __tablename__ = "teams"
+    __tablename__ = "books"
     id = Column(Integer, primary_key=True, autoincrement= True)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
     category = Column(String, nullable=False)
-    quantity = Column(String, nullable= False)
+    quantity = Column(, nullable= False)
     
